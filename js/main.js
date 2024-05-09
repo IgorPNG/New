0da670912266c26698958b9ed705be7b82f4fd4b
IgorPNG <166862209+IgorPNG@users.noreply.github.com>
2024-05-09 18:00:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,13 +6,64 @@ var app = new Vue({
         { id: 3, title: "Cucumber3", short_text: 'Crystal Cucumber.', image: 'images/Crystal.jpg', desc: "This cucumber with an elongated name only grows to about 5-6 inches. This small white cucumber does not look at all like a cucumber with a much rounder shape. They are generally used as a slicing cucumber and not often pickled." },
         { id: 4, title: "Cucumber4", short_text: 'Armenian Cucumber.', image: 'images/Armenian.jpg', desc: "Armenian cucumbers, also known as snake cucumbers or yard-long cucumbers, are long and thin with a pale green skin and few seeds. They have a mild, sweet flavor and a crunchy texture. Armenian cucumbers are a popular choice for salads and sandwiches, but they can also be used for pickling. They are often harvested when they are about 12 inches or so as they quickly become bitter with growth." },
         { id: 5, title: "Cucumber5", short_text: 'English Cucumber', image: 'images/English.jpg', desc: "English cucumbers, also known as seedless cucumbers or hot house cucumbers, are long and narrow with a thin skin and few seeds.They are usually wrapped in plastic to protect them during transport and storage. English cucumbers are a popular choice for salads and sandwiches because of their mild flavor and crunchy texture. They are also great for pickling because they hold their shape well. Together with American cucumbers, English cucumbers are the two most commonly available cucumbers at most groceries." }],
+        btnVisible: 0,
+        product: {},
+        cart: [],
+        contactFields: [],
+        orderSubmitted: false
     },
     mounted: function () {
         console.log(window.localStorage.getItem('prod'));
+        this.getProduct();
+        this.checkInCart();
+        this.getCart();
     },
     methods: {
+        checkInCart: function () {
+            if (this.product && this.product.id && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id)) != -1) this.btnVisible = 1;
+        },
         addItem: function (id) {
             window.localStorage.setItem('prod', id);
+        },
+        getProduct: function () {
+            if (window.location.hash) {
+                var id = window.location.hash.replace('#', '');
+                if (this.products && this.products.length > 0) {
+                    for (i in this.products) {
+                        if (this.products[i] && this.products[i].id && id == this.products[i].id) this.product = this.products[i];
+                    }
+                }
+            }
+        },
+        addToCart: function (id) {
+            var cart = [];
+            if (window.localStorage.getItem('cart')) {
+                cart = window.localStorage.getItem('cart').split(',');
+            }
+            if (cart.indexOf(String(id)) == -1) {
+                cart.push(id);
+                window.localStorage.setItem('cart', cart.join());
+                this.btnVisible = 1;
+            }
+        },
+        getCart: function () {
+            var cartIds = window.localStorage.getItem('cart').split(',');
+            this.cart = this.products.filter(product => cartIds.includes(String(product.id)));
+        },
+        removeFromCart: function (id) {
+            var cart = window.localStorage.getItem('cart').split(',');
+            var index = cart.indexOf(String(id));
+            if (index !== -1) {
+                cart.splice(index, 1);
+                window.localStorage.setItem('cart', cart.join());
+                this.cart = [];
+                this.getCart();
+            }
+        },
+        makeOrder() {
+            this.cart = [];
+            localStorage.removeItem('cart');
+            this.orderSubmitted = true;
         }
     }
 });
